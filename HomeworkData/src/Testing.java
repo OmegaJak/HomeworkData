a7b175b86d1903706641925700c5c60067df75cf
OmegaJak <jak.kruger@gmail.com>
2015-01-09 17:37:47 -0600

    Working on methodization

--- HomeworkData/src/Testing.java
+++ HomeworkData/src/Testing.java
@@ -23,25 +23,22 @@ public class Testing {
 			ArrayList<String[]> rows = new ArrayList<String[]>();
 			String curLine[] = {};
 			while ((line = reader.readLine()) != null) {//Go through each line sequentially until there are no more
-				// System.out.println(line);
 				curLine = line.split(","); //Creates an array of each element between the commas
 				if (line.charAt(0) != ',') {//Ignore empty rows
 					rows.add(curLine);
 				}
 			}
 
-			/*for (int i = 0; i < rows.size(); i++) { //Loop through the ArrayList
-				for (int k = 0; k < rows.get(i).length; k++) {//through the inner array, i.e., looping through the columns of the current row
-					System.out.print(rows.get(i)[k] + ",  ");
-				}
-				System.out.println("");
-			}*/
+			/* for (int i = 0; i < rows.size(); i++) { //Loop through the
+			 * ArrayList for (int k = 0; k < rows.get(i).length; k++) {//through
+			 * the inner array, i.e., looping through the columns of the current
+			 * row System.out.print(rows.get(i)[k] + ",  "); }
+			 * System.out.println(""); } */
 
 			System.out.println(rows.size());
 
-			/*for (int i = 0; i < rows.size(); i++) {
-				System.out.println(rows.get(i)[2]);
-			}*/
+			/* for (int i = 0; i < rows.size(); i++) {
+			 * System.out.println(rows.get(i)[2]); } */
 
 			reader.close();
 
@@ -51,11 +48,12 @@ public class Testing {
 		}
 	}
 
-	/**
-	 * @param row The row you want to edit - 1
-	 * @param column The column you want to edit - 1
-	 * @param fill The string to fill the cell with
-	 */
+	/** @param row
+	 *            The row you want to edit - 1
+	 * @param column
+	 *            The column you want to edit - 1
+	 * @param fill
+	 *            The string to fill the cell with */
 	private static void writeCell(int row, int column, String fill) {
 		try {
 			Path path = FileSystems.getDefault().getPath(
@@ -67,30 +65,28 @@ public class Testing {
 			ArrayList<String[]> rows = new ArrayList<String[]>();
 			String curLine[] = {};
 			while ((line = reader.readLine()) != null) {//Go through each line sequentially until there are no more
-				// System.out.println(line);
 				curLine = line.split(",", -10); //Creates an array of each element between the commas
 				rows.add(curLine);
-				System.out.println("Hello");
 			}
 
 			PrintWriter pw = new PrintWriter(new FileWriter("test.csv"));
-			
-			System.out.println("writeTest()");
+
+			System.out.println("writeCell()");
 			System.out.println(rows.size());
-			
+
 			for (int i = 0; i < rows.size(); i++) {
-				System.out.println("i is: " + i);
-				System.out.println("row length is: " + rows.get(i).length);
+				//System.out.println("i is: " + i);
+				//System.out.println("row length is: " + rows.get(i).length);
 				for (int k = 0; k < rows.get(i).length; k++) {
 					//pw.write(",,,,,,,,\n");
-					System.out.println("k is: " + k);
+					//System.out.println("k is: " + k);
 					if (i == row && k == column) { //A specific cell, row 4 column 3
 						pw.write(fill + ",");
-					}else{
-						System.out.println("Wassup");
+					} else {
+						//System.out.println("Wassup");
 						if (k == rows.get(i).length - 1) { //If this is the last cell in the row
 							pw.write(rows.get(i)[k] + "\n");
-						}else{
+						} else {
 							pw.write(rows.get(i)[k] + ",");
 						}
 					}
@@ -100,6 +96,25 @@ public class Testing {
 			pw.close();
 		} catch (IOException e) {
 			System.out.println("Ruh roh...");
+		}
+	}
+
+	public static void readFile(String dir, String file, boolean ignoreEmptyLines) {
+		try {
+			Path path = FileSystems.getDefault().getPath(dir, file);
+			BufferedReader reader = Files.newBufferedReader(path, charset);
+			String line = null;
+
+			ArrayList<String[]> rows = new ArrayList<String[]>();
+			String curLine[] = {};
+			while ((line = reader.readLine()) != null) {//Go through each line sequentially until there are no more
+				curLine = line.split(","); //Creates an array of each element between the commas
+				if (line.charAt(0) != ',') {//Ignore empty rows
+					rows.add(curLine);
+				}
+			}
+		} catch (IOException ioex) {
+			System.out.println("There was an IOExceptiojn. Stahp.");
 		}
 	}
 
