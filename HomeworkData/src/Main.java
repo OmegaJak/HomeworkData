e5cedb9d65dc9b4d280a7b9bd1a94da59b86edc1
OmegaJak <jak.kruger@gmail.com>
2015-01-12 18:27:29 -0600

    Changed directories to windows as a temporary fix, completed timePerUnit, and began working on convertTimeToDouble (it's functional but needs to be more flexible).

--- HomeworkData/src/Main.java
+++ HomeworkData/src/Main.java
@@ -11,16 +11,25 @@ import java.util.ArrayList;
 public class Main {
 
 	private static Charset charset = Charset.forName("US-ASCII");
+	public static String csvDir = "";
+	public static String csvName = "";
 
 	public static void main(String[] args) {
 		try {
-			ArrayList<String[]> rows = readFile("/home/jak/Programming/HomeworkData/HomeworkData/bin", "HomeworkData.csv", false);
+			//csvDir = "/home/jak/Programming/HomeworkData/HomeworkData/bin";
+			csvDir = "C:\\Users\\JAK\\Programming\\Other Random Java\\HomeworkData";
+			csvName = "HomeworkData.csv";
+			
+			ArrayList<String[]> rows = readFile(csvDir, csvName, false);
 
-			for (int i = 0; i < rows.size(); i++) { //Loop through theArrayList 
-				for (int k = 0; k < rows.get(i).length; k++) {//through the inner array, i.e., looping through the columns of the current row 
-					System.out.print(rows.get(i)[k] + ",  \t\t");
+			boolean switchBoolean = false;
+			if (switchBoolean) {
+				for (int i = 0; i < rows.size(); i++) { //Loop through theArrayList 
+					for (int k = 0; k < rows.get(i).length; k++) {//through the inner array, i.e., looping through the columns of the current row 
+						System.out.print(rows.get(i)[k] + ",  \t\t");
+					}
+					System.out.println("");
 				}
-				System.out.println("");
 			}
 
 			System.out.println(rows.size());
@@ -29,12 +38,16 @@ public class Main {
 				System.out.println(rows.get(i)[2]);
 			}*/
 
-			writeCell(6, 4, "Test", "/home/jak/Programming/HomeworkData/HomeworkData", "test.csv", false);
+			writeCell(6, 4, "Test", csvDir + "\\HomeworkData", "test.csv", false);
+			
+			timePerUnit(readFile(csvDir, csvName, false), 4);
+			
 		} catch (IOException e) {
 			System.out.println("There was an IOException somewhere. Stahp.");
-			System.out.println(e.getMessage());
-			System.out.println(e.getCause());
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Something went wrong, who the hell knows...");
+			e.printStackTrace();
 		}
 	}
 
@@ -59,10 +72,13 @@ public class Main {
 
 		ArrayList<String[]> rows = readFile(dir, file, true);
 
-		PrintWriter pw = new PrintWriter(new FileWriter(isMakingACopy ? "backup-" + file : file));
+		PrintWriter pw = new PrintWriter(new FileWriter(isMakingACopy ? "Backup-" + file : file));
 
-		System.out.println("writeCell()");
-		System.out.println(rows.size());
+		if (!isMakingACopy) {
+			System.out.println("writeCell()");
+			System.out.println(rows.size());
+			System.out.println("Writing \"" + fill + "\" to row " + row + ", column " + column + " of " + file);
+		}
 
 		for (int i = 0; i < rows.size(); i++) {
 			for (int k = 0; k < rows.get(i).length; k++) {
@@ -87,30 +103,63 @@ public class Main {
 	 * @param allowEmptyLines - If this is false, it will only add lines that have something in the first cell
 	 * @return An Arraylist of String arrays, with each String array being a row of the csv file, each index of each array a cell
 	 */
-	public static ArrayList<String[]> readFile(String dir, String file, boolean allowEmptyLines) throws IOException {
-		Path path = FileSystems.getDefault().getPath(dir, file); //The path to the file, needed for newBufferedReader()
-		BufferedReader reader = Files.newBufferedReader(path, charset);
-		String line = null;
+	public static ArrayList<String[]> readFile(String dir, String file, boolean allowEmptyLines) {
+		try {
+			Path path = FileSystems.getDefault().getPath(dir, file); //The path to the file, needed for newBufferedReader()
 
-		ArrayList<String[]> rows = new ArrayList<String[]>();
-		String curLine[] = {};
-		while ((line = reader.readLine()) != null) {//Go through each line sequentially until there are no more
-			curLine = line.split(",", -1); //Creates an array of each element between the commas
-			if (allowEmptyLines || line.charAt(0) != ',') {//Ignore empty rows
-				rows.add(curLine);
+			BufferedReader reader = Files.newBufferedReader(path, charset);
+			String line = null;
+
+			ArrayList<String[]> rows = new ArrayList<String[]>();
+			String curLine[] = {};
+			while ((line = reader.readLine()) != null) {//Go through each line sequentially until there are no more
+				curLine = line.split(",", -1); //Creates an array of each element between the commas
+				if (allowEmptyLines || line.charAt(0) != ',') {//Ignore empty rows
+					rows.add(curLine);
+				}
 			}
-		}
 
-		reader.close();
-		return rows;
+			reader.close();
+			return rows;
+		} catch (IOException e) {
+			System.out.println("There was an error while reading the file!");
+			System.out.println(e.getLocalizedMessage());
+		}
+		return new ArrayList<String[]>();
 	}
 
 	//-------------------------------------------------------------------------------------//
 	//--------------------------------Data analysis methods--------------------------------//
 	//-------------------------------------------------------------------------------------//
-	
-	private static boolean timePerUnit(ArrayList<String[]> dataSheet, int row) {
-		return false;
-	}
 
+	private static ArrayList<String[]> timePerUnit(ArrayList<String[]> dataSheet, int row) throws IOException{
+		double calculatedResult = convertTimeToDouble(dataSheet.get(row)[7]) / Double.parseDouble(dataSheet.get(row)[4]);
+		writeCell(row, 5, String.valueOf(calculatedResult), csvDir, csvName, false);
+		return null;
+	}
+	
+	//------------------------------------------------------------------------------------//
+	//--------------------------------Other Helper Methods--------------------------------//
+	//------------------------------------------------------------------------------------//
+	
+	/**
+	 * 
+	 * @param time Needs to be in the format of H:MM
+	 * @return The total minutes in the time provided
+	 */
+	public static double convertTimeToDouble(String time) {
+		String minutes = time.substring(time.length() - 2, time.length());
+		String hours = time.substring(0, 1);
+		
+		double total = 0;
+		
+		try {
+			total = (Double.parseDouble(hours) * 60) + Double.parseDouble(minutes);
+		} catch (NumberFormatException e) {
+			System.out.println("Them numbers ain't right...");
+			e.printStackTrace();
+		}
+		
+		return total;
+	}
 }
